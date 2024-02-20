b279e646836d1e04eb0f488a317501db318b28e0
uqarni <108438834+uqarni@users.noreply.github.com>
2024-02-21 00:08:43 +0300

    Update db.py

--- db.py
+++ db.py
@@ -14,7 +14,7 @@ def initialize_prompt_and_text(session_state):
     initial_text = bot_info['initial_text']
     to_format = {
         'lead_first_name': session_state.lead_first_name,
-        'agent_name': 'Spencer',
+        'agent_name': 'Andy',
         'industry': session_state.industry,
         'company_name': session_state.company_name,
         'booking_link': 'array_booking.com'
